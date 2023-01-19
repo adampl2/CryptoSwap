9c0965e8fdea83cdc1c4cb02e9d9464cd69b0c56
adampl2 <adamchwiulkowski@gmail.com>
2023-01-19 03:52:30 +0000

    HTML basic structure completed and styled. Js code made more to be more efficient.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,27 @@
-
 // Redirects the user to desired link when clicked on a navbar button
 
-let buttons = document.getElementsByClassName("nav-button");
+let buttons = document.querySelectorAll(".nav-button");
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
+buttons.forEach(function(button) {
+  button.addEventListener("click", function () {
     let link = this.getAttribute("data-href");
     window.open(link, "_blank");
   });
-}
+});
 
 // Changes the content when clicked on a different tab in the "Learn more about crypto" section.
 
 const tabs = document.querySelectorAll('[data-tab-value]');
-const tabInfos = document.querySelectorAll('[data-tab-info]');
+const tabInfos = document.querySelectorAll('.tabs__tab ');
 
 tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document
-      .querySelector(tab.dataset.tabValue);
-
+  tab.addEventListener('click', event => {
+    const target = document.querySelector(event.target.dataset.tabValue);
     tabInfos.forEach(tabInfo => {
-      tabInfo.classList.remove('active')
-    })
-    target.classList.add('active');
-  })
-})
+      tabInfo.classList.remove('active');
+      if (tabInfo.id === target.id) {
+        tabInfo.classList.add('active');
+      }
+    });
+  });
+});
