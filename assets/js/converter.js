eb3ebcf628b92a8477b1944069dfdcc20f9f12f1
adampl2 <adamchwiulkowski@gmail.com>
2023-01-27 02:04:48 +0000

    live price added.

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -1,11 +1,16 @@
-
-let ws = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
+let eth = document.getElementById("ethereum");
 let btc = document.getElementById("bitcoin");
-let lastPrice = null;
+let ethWs = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
+let btcWs = new WebSocket('wss://stream.binance.com:9443/ws/btceur@trade');
 
-
-ws.onmessage = (event) => {
+ethWs.onmessage = (event) => {
   let stockObject = JSON.parse(event.data);
   let price = parseFloat(stockObject.p).toFixed(2);
-  btc.innerText = price; 
+  eth.innerText = price;
+}
+
+btcWs.onmessage = (event) => {
+  let stockObject = JSON.parse(event.data);
+  let price = parseFloat(stockObject.p).toFixed(2);
+  btc.innerText = price;
 }
