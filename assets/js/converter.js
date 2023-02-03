4f1721d9575753d6597b2fdd2314f0be74c8a76b
adampl2 <adamchwiulkowski@gmail.com>
2023-02-03 13:29:27 +0000

    converter.js: Test

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -11,7 +11,7 @@ let lastPrice = null;
 
 // Stores the currency pairs to avoid calling the handlePrice 6 times.
 
-let cryptoArray = [{
+const cryptoArray = [{
     abr: "etheur",
     crypto: eth
   },
